ccd2f8e2c361d81e54c66937eec4431d31869bd4
Donovan Palmer <donovan.palmer@umusic.com>
2016-08-05 17:27:26 -0700

    dynamic migrations working.

--- src/Form/MigrateYouTubeSource.php
+++ src/Form/MigrateYouTubeSource.php
@@ -4,6 +4,7 @@ namespace Drupal\migrate_youtube\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\migrate_youtube\YouTubeMigrationGenerator;
 
 /**
  * Class MigrateYouTubeSource.
@@ -33,6 +34,20 @@ class MigrateYouTubeSource extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('migrate_youtube.migrateyoutubesource');
+
+    // API Key
+    $form['youtube_channel'] = array(
+      '#type' => 'textfield',
+      '#title' => 'Youtube Channel Name or ID',
+      '#description' => t('e.g. KendrickLamarVevo'),
+    );
+    //  Author
+    $form['author'] = array(
+      '#type' => 'textfield',
+      '#title' => 'Author',
+      '#description' => t('e.g. Author'),
+    );
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -49,6 +64,12 @@ class MigrateYouTubeSource extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
+    $configuration = [];
+    $configuration['youtube_channel'] = $form_state->getValue('youtube_channel');
+    $configuration['author'] = $form_state->getValue('author');
+
+    $generator = new YouTubeMigrationGenerator($configuration);
+    $generator->createMigrations();
     $this->config('migrate_youtube.migrateyoutubesource')
       ->save();
   }
---
+++ src/YouTubeMigrationGenerator.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Drupal\migrate_youtube;
+
+use Drupal\migrate_plus\Entity\Migration;
+use Drupal\migrate_plus\Entity\MigrationGroup;
+use Drupal\migrate\Plugin\MigrationPluginManagerInterface;
+
+/**
+ * Functionality to construct YouTube Migrations from broad configuration
+ * settings.
+ */
+class YouTubeMigrationGenerator {
+
+  /**
+   * Configuration to guide our migration creation process.
+   *
+   * @var array
+   */
+  protected $configuration = [];
+
+  /**
+   * Constructs a WordPress migration generator, using provided configuration.
+   *
+   * @todo: Validate inputs (e.g., make sure post type exists).
+   * @link https://www.drupal.org/node/2742283
+   *
+   * @param $configuration
+   *   An associative array:
+   *   - file_uri: Drupal stream wrapper of the source WordPress XML file.
+   *   - group_id: ID of the MigrationGroup holding the generated migrations.
+   *   - prefix: String to prefix to the IDs of generated migrations.
+   *   - default_author: If present, username to author all imported content. If
+   *     absent or empty, users will be imported from WordPress.
+   *   - tag_vocabulary: Machine name of vocabulary to hold tags.
+   *   - category_vocabulary: Machine name of vocabulary to hold categories.
+   *   - [post|page]: Associative array of type-specific configuration:
+   *     - type: Machine name of Drupal node bundle to hold content.
+   *     - text_format: Machine name of text format for body field.
+   */
+  public function __construct(array $configuration) {
+    $this->configuration = $configuration;
+  }
+  
+  /**
+   * Creates a set of WordPress import migrations based on configuration settings.
+   */
+  public function createMigrations() {
+
+    $dependencies = [];
+    $content_id = 'YoutubeMigration' . $this->configuration['youtube_channel'];
+    $migration = static::createEntityFromPlugin('youtube_videos', $content_id);
+    ksm($migration);
+    $migration->set('migration_group', 'youtube');
+    $source = $migration->get('source');
+    $source['id'] = $this->configuration['youtube_channel'];
+    $migration->set('source', $source);
+
+    $migration->set('migration_dependencies', ['required' => $dependencies]);
+    $migration->save();
+
+  }
+
+  /**
+   * Create a configuration entity from a core migration plugin's configuration.
+   *
+   * @todo: Remove and replace calls to use Migration::createEntityFromPlugin()
+   * when there's a migrate_plus release containing it we can have a dependency
+   * on.
+   *
+   * @param string $plugin_id
+   *   ID of a migration plugin managed by MigrationPluginManager.
+   * @param string $new_plugin_id
+   *   ID to use for the new configuration entity.
+   *
+   * @return \Drupal\migrate_plus\Entity\MigrationInterface
+   *   A Migration configuration entity (not saved to persistent storage).
+   */
+  protected static function createEntityFromPlugin($plugin_id, $new_plugin_id) {
+    /** @var \Drupal\migrate\Plugin\MigrationPluginManagerInterface $plugin_manager */
+    $plugin_manager = \Drupal::service('plugin.manager.migration');
+    $migration_plugin = $plugin_manager->createInstance($plugin_id);
+    ksm($migration_plugin);
+    $entity_array['id'] = $new_plugin_id;
+    $entity_array['label'] = $migration_plugin->label();
+    $entity_array['source'] = $migration_plugin->getSourceConfiguration();
+    $entity_array['destination'] = $migration_plugin->getDestinationConfiguration();
+    $entity_array['process'] = $migration_plugin->getProcess();
+    $entity_array['migration_dependencies'] = $migration_plugin->getMigrationDependencies();
+    ksm($entity_array);
+    $migration_entity = Migration::create($entity_array);
+    return $migration_entity;
+  }
+}
